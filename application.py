c3ff1fb9f1b901cffc7143e9c86c4d0b655f80f3
anikoBarat <aniko.barat@gmail.com>
2017-08-19 19:48:11 +0200

    An imroved solution to how to handle with .txt

--- application.py
+++ application.py
@@ -1,18 +1,14 @@
 from flask import Flask, redirect, request, session, render_template
 
+request_counter = {"GET": 0, "POST": 0, "PUT": 0, "DELETE": 0}
+
 app = Flask(__name__)
 
 @app.route("/request-counter", methods=["GET", "POST", "PUT", "DELETE"])
 def route_request_counter():
-    request_counter = {}  
-    with open("request_counts.txt", "r") as f:
-        for line in f:
-            line = line.split(":")
-            request_counter[line[0]] = int(line[1])          
-    
     if request.method in request_counter:
         request_counter[request.method] += 1
-                    
+                
     with open("request_counts.txt", "w") as f:
         for i in request_counter:
             line = i + ":" + str(request_counter[i]) + "\n"
@@ -27,7 +23,6 @@ def route_statistics():
         for line in f:
             line = line.split(":")
             content_to_render.append(line)
-        print(content_to_render)
     return render_template("statistics.html", content=content_to_render)
 
 
